3a5c61fa991083fd56739cb792cd771f96389f8a
Priyanshu Srivastava <priyanshusrivastava770@gmail.com>
2024-05-03 10:17:35 +0530

    commit

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -6,7 +6,6 @@ import { FaRegUser } from "react-icons/fa";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { Context } from "../../main";
-import ReactTyped from "react-typed";
 
 const Login = () => {
   const [email, setEmail] = useState("");
